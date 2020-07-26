434d23eaa1a52dd76784b8a5741592d97c8308f5
JoeySuttonPreece <102568393@student.swin.edu.au>
2020-07-26 12:24:05 +1000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EDSLPrototype
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Page seasonPage;
+        private Page resultPage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickSeason(object sender, RoutedEventArgs e)
+        {
+            if (seasonPage == null)
+            {
+                seasonPage = new Season();
+            }
+            Main.Content = seasonPage;
+        }
+
+        private void ClickResult(object sender, RoutedEventArgs e)
+        {
+            if (resultPage == null)
+            {
+                resultPage = new Result();
+            }
+            Main.Content = resultPage;
+        }
+    }
+}
